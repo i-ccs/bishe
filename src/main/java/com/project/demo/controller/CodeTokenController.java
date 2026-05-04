31ad9b886582fd2294139bf725ba652d3cc4174a
yuan <yuan@debian.debian>
2026-05-04 19:09:00 +0800

    javaname

--- src/main/java/com/project/demo/controller/CodeTokenController.java
+++ src/main/java/com/project/demo/controller/CodeTokenController.java
@@ -39,12 +39,12 @@ public class CodeTokenController extends BaseController<CodeToken, CodeTokenServ
      */
     @GetMapping("send_email")
     public Map<String, Object> sendEmail(String email) {
-        //判断邮箱号是否为空
+        // 判断邮箱号是否为空
         if (StringUtils.isEmpty(email)) {
             return error(30000, "参数错误");
         }
         try {
-            //随机生成 6位数的验证码
+            // 随机生成 6位数的验证码
             int code = (int) (Math.floor(Math.random() * 900000) + 100000);
 
             // 创建MimeMessage对象
@@ -54,60 +54,58 @@ public class CodeTokenController extends BaseController<CodeToken, CodeTokenServ
             MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
 
             // 设置收件人、发件人、主题和内容
-            helper.setFrom("3216252476@qq.com");  // 替换为你的QQ邮箱地址
+            helper.setFrom("3216252476@qq.com"); // 替换为你的QQ邮箱地址
             helper.setTo(email);
             helper.setSubject("找回密码");
-            helper.setText("<h1>验证码为："+code+"</h1>", true);
+            helper.setText("<h1>验证码为：" + code + "</h1>", true);
 
             // 发送邮件
             mailSender.send(mimeMessage);
             Calendar cal = Calendar.getInstance();
-            cal.add(Calendar.MINUTE,10);
+            cal.add(Calendar.MINUTE, 10);
             String token = service.encryption(code + "_" + cal.getTime());
             SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
             Map<String, Object> map = new HashMap<>();
-            map.put("token",token);
-            map.put("code",code);
-            map.put("expire_time",format.format(cal.getTime()));
+            map.put("token", token);
+            map.put("code", code);
+            map.put("expire_time", format.format(cal.getTime()));
             this.addMap(map);
             Map<String, Object> result = new HashMap<>();
-            result.put("result",1);
-            result.put("token",token);
+            result.put("result", 1);
+            result.put("token", token);
             return result;
-        }catch (MessagingException e){
+        } catch (MessagingException e) {
             e.printStackTrace();
             return error(30000, e.getMessage());
         }
     }
 
     @GetMapping("verify_email_code")
-    public Map<String, Object> verifyEmailCode(String token,String code) {
+    public Map<String, Object> verifyEmailCode(String token, String code) {
         if (StringUtils.isEmpty(token) || StringUtils.isEmpty(code)) {
             return error(30000, "参数错误");
-        }else {
-            Map<String,String> queryMap = new HashMap<>();
-            queryMap.put("token",token);
-            Map<String,String> configMap = new HashMap<>();
-            configMap.put("like","false");
+        } else {
+            Map<String, String> queryMap = new HashMap<>();
+            queryMap.put("token", token);
+            Map<String, String> configMap = new HashMap<>();
+            configMap.put("like", "false");
             List resultList = service.selectBaseList(service.select(queryMap, configMap));
             if (resultList.size() > 0) {
                 CodeToken obj = (CodeToken) resultList.get(0);
                 Date now = new Date();
-                if (obj.getExpire_time().after(now)){
-                    if (code.equals(obj.getCode())){
+                if (obj.getExpireTime().after(now)) {
+                    if (code.equals(obj.getCode())) {
                         return success(1);
-                    }else {
+                    } else {
                         return error(30000, "验证码错误");
                     }
-                }else {
+                } else {
                     return error(30000, "验证码已失效");
                 }
-            }else {
+            } else {
                 return error(30000, "token请求错误");
             }
         }
     }
 
 }
-
-
