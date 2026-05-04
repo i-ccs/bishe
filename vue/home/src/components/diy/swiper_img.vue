e2ab375ad9ba02bad02be0d2804543594991f1fd
yuan <yuan@debian.debian>
2026-05-04 15:02:10 +0800

    HOME

--- vue/home/src/components/diy/swiper_img.vue
+++ vue/home/src/components/diy/swiper_img.vue
@@ -73,14 +73,72 @@ export default {
 </script>
 
 <style scoped>
-	.swiper-imgs{
-		background-repeat: no-repeat;
-		background-size: cover;
-		height: 18.75rem;
+#carousel {
+	height: 100%;
+}
+
+.swiper-imgs {
+	background-repeat: no-repeat;
+	background-size: cover;
+	background-position: center;
+	height: 100%; 
+	min-height: 300px;
+	transition: transform 0.5s ease;
+}
+
+/* Custom Indicators */
+#carousel >>> .carousel-indicators {
+	bottom: 20px;
+}
+
+#carousel >>> .carousel-indicators li {
+	width: 12px;
+	height: 4px;
+	border-radius: 2px;
+	background-color: rgba(255, 255, 255, 0.5);
+	border: none;
+	margin: 0 4px;
+	transition: all 0.3s ease;
+}
+
+#carousel >>> .carousel-indicators li.active {
+	width: 24px;
+	background-color: #fff;
+}
+
+/* Custom Controls */
+#carousel >>> .carousel-control-prev,
+#carousel >>> .carousel-control-next {
+	width: 40px;
+	height: 40px;
+	background: rgba(0, 0, 0, 0.2);
+	border-radius: 50%;
+	top: 50%;
+	transform: translateY(-50%);
+	margin: 0 20px;
+	opacity: 0;
+	transition: all 0.3s ease;
+}
+
+#carousel:hover >>> .carousel-control-prev,
+#carousel:hover >>> .carousel-control-next {
+	opacity: 1;
+}
+
+#carousel >>> .carousel-control-prev:hover,
+#carousel >>> .carousel-control-next:hover {
+	background: rgba(0, 0, 0, 0.4);
+}
+
+#carousel >>> .carousel-control-prev-icon,
+#carousel >>> .carousel-control-next-icon {
+	width: 20px;
+	height: 20px;
+}
+
+@media (max-width: 996px) {
+	.swiper-imgs {
+		height: 250px;
 	}
-	@media (max-width: 996px) {
-	    .swiper-imgs {
-	        height: 12.5rem;
-	    }
-	}
+}
 </style>
--- vue/home/src/views/index.vue
+++ vue/home/src/views/index.vue
@@ -1,126 +1,68 @@
 <template>
 	<div class="page_root" id="root_demo">
+		<!-- Hero Section -->
 		<div class="warp banner">
-		    <div class="container swiper_box">
-		        <div class="row">
-		            <div class="col-12">
-		                <div class="sw_no">
-		                    <!-- 轮播图组件 -->
-		                    <div class="swiper_img">
-		                        <swiper_img :list="list_slide"/>
-		                    </div>
-							<div class="card_notice">
-								<div class="notice_title">公告信息 <router-link to="notice/list" class="not_more more"><span class="mor">更多</span></router-link></div>
-								<!-- 公告组件 -->
+			<div class="container swiper_box">
+				<!-- Centered Swiper Row -->
+				<div class="row justify-content-center">
+					<div class="col-lg-4 col-md-6 col-sm-12">
+						<div class="premium-swiper-container">
+							<div class="swiper_img premium-swiper">
+								<swiper_img :list="list_slide"/>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!-- Notice Row -->
+				<div class="row justify-content-center mt-4">
+					<div class="col-lg-8 col-12">
+						<div class="card_notice premium-notice-card wide-notice">
+							<div class="notice_header">
+								<span class="notice_title_text">最新公告</span>
+								<router-link to="notice/list" class="notice_more">更多 <i class="el-icon-arrow-right"></i></router-link>
+							</div>
+							<div class="notice_body">
 								<swiper_notice :list="list_notice"/>
 							</div>
-		                </div>
-		            </div>
-		        </div>
-		    </div>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 
-
-		<div class="warp article_s">
-		    <!-- 容器 -->
-		    <div class="container">
-		        <div class="row">
-		            <div class="col-12">
-		                <!-- 标题栏组件 -->
-		                <div class="article_recommend">
-		                    <div class="title">
-		                        <span> 行业资讯 </span>
-		                    </div>
-							<div class="text_t"><span></span></div>
-		                    <div class="more_box">
-		                        <router-link to="/article/list" class="art_more more">
-		                            <span class="mor">更多</span>
-		                        </router-link>
-		                    </div>
-
-		                </div>
-						<div class="list_article_box">
-						<div class="Left_box">
-								<span class="i"></span>
-								<span class="i"></span>
-								<span class="i"></span>
-                        </div>
-							<!-- 行业资讯列表组件 -->
-							<list_article v-if="show_list_article" :list="list_article" />
-							<div class="overflow-auto table_article" >
-								<table id="list_article" role="table" aria-busy="false"
-									:aria-colcount="fields_article.length"
-									class="table b-table table-striped table-hover">
-									<thead>
-									<tr>
-										<th v-for="(o,i) in fields_article" :key="i">
-											{{o.label}}
-										</th>
-									</tr>
-									</thead>
-									<tbody>
-									<tr v-for="(o, i) in list_table_article" :key="i">
-										<td v-for="(oj,n) in fields_article" @click="to_details('article',o)">
-											<img v-if="oj.type && oj.type == '图片' " :src="$fullUrl(o[oj.key])" alt=""
-												 v-default-img="'../../public/img/default.png'">
-											<span v-else-if="oj.key === 'create_time'">{{ o[oj.key] | formatDate }}</span>
-											<span v-else>{{ o[oj.key] }}</span>
-										</td>
-									</tr>
-									</tbody>
-								</table>
+		<!-- Main Content Section (News & Products) -->
+		<div class="home-section">
+			<div class="container">
+				<el-card class="unified-content-card">
+					<el-row :gutter="40">
+						<!-- Left: Industry News -->
+						<el-col :lg="12" :md="12" :sm="24">
+							<div class="content-column news-column">
+								<div class="section-header mini">
+									<div class="title">行业资讯</div>
+									<router-link to="/article/list" class="more-link">更多 <i class="el-icon-arrow-right"></i></router-link>
+								</div>
+								<div class="list_article_box">
+									<list_article v-if="show_list_article" :list="list_article" />
+								</div>
 							</div>
-							<div class="right_box">
-								<span class="i"></span>
-								<span class="i"></span>
-								<span class="i"></span>
-                        	</div>
-						</div>
-
-		            </div>
-		        </div>
-		    </div>
+						</el-col>
+						<!-- Right: Featured Products -->
+						<el-col :lg="12" :md="12" :sm="24">
+							<div class="content-column products-column" v-if="$check_action('/commodity_information/list', 'get')">
+								<div class="section-header mini">
+									<div class="title">商品信息推荐</div>
+									<router-link to="/commodity_information/list" class="more-link">更多 <i class="el-icon-arrow-right"></i></router-link>
+								</div>
+								<div class="list_commodity_information_box">
+									<list_commodity_information :list="list_commodity_information" />
+								</div>
+							</div>
+						</el-col>
+					</el-row>
+				</el-card>
+			</div>
 		</div>
-				<!-- 推荐商品信息模块(开始) -->
-		<div class="ins warp model1" v-if="$check_action('/commodity_information/list', 'get')">
-		    <!-- 容器 -->
-		    <div class="container">
-		        <div class="row">
-		            <div class="col-12">
-		                <!-- 标题栏组件 -->
-		                <div class="diy_recommend">
-		                    <div class="title">
-		                        <span> 商品信息推荐 </span>
-		                    </div>
-							<div class="text_t"><span></span></div>
-		                    <div class="more_box">
-		                        <router-link to="/commodity_information/list" class="ins_more more">
-		                            <span calss="mor">更多</span>
-		                        </router-link>
-		                    </div>
-
-		                </div>
-		                <!-- 商品信息推荐列表组件 -->
-						<div class="list_commodity_information_box" ins_box>
-							<div class="Left_box">
-								<span class="i"></span>
-								<span class="i"></span>
-								<span class="i"></span>
-                        	</div>
-							<list_commodity_information :list="list_commodity_information" />
-							<div class="right_box">
-								<span class="i"></span>
-								<span class="i"></span>
-								<span class="i"></span>
-                        	</div>
-						</div>
-
-		            </div>
-		        </div>
-		    </div>
-		</div>
-		<!-- 推荐商品信息模块(结束) -->
-									
 	</div>
 </template>
 
@@ -357,348 +299,123 @@
 </script>
 
 <style scoped>
-.page_root{
-	position: relative;
+.page_root {
+	background-color: var(--portal-bg);
+	min-height: 100vh;
 }
-.support_entry{
-	position: fixed;
-	top: 400px;
-	right: 20px;
-	width: 80px;
-	height: 80px;
-	cursor: pointer;
-	z-index: 99999;
+
+/* Hero Section */
+.banner {
+	padding-top: 20px;
 }
-.support_module .container{
-        /* height: 650px; */
-        height: 70%;
-        width: 700px;
-        border-radius: 4px;
-        border: 0.5px solid #e0e0e0;
-        background-color: #f5f5f5;
-        /* display: flex;
-        flex-flow: column; */
-        overflow: hidden;
-				position: fixed;
-				/* top: 100px; */
-				padding: 0;
-				margin-left: -350px;
-				z-index: 99999999;
-    }
-	.support_module	.small_css{
-		height: 40px;
-		width: 200px;
-		right: 0;
-		bottom: 0px;
-	}
-	.support_module	.big_css{
-				top: 60px;
-				left: 50%;
-				}
-    .support_module .content{
-        width: calc(100% - 0px);
-        padding: 20px 30px;
-        overflow-y: scroll;
-				height: 70%;
-    }
-		.support_module .content::-webkit-scrollbar{
-	display: none
+
+.premium-swiper-container {
+	margin: 0 auto;
 }
-.support_module_title{
-	text-align: right;
-	height: 40px;
-	line-height: 40px;
-	border-bottom: 1px solid #ccc;
 
+.premium-swiper {
+	height: 300px; /* Adjusted height for 1/3 width */
+	border-radius: var(--portal-radius);
+	overflow: hidden;
+	box-shadow: var(--portal-shadow);
 }
-.support_module_title .title_btn{
-	font-size: 20px;
-	cursor: pointer;
-	margin-right: 20px;
-	color: #888;
 
+.premium-notice-card {
+	background: #fff;
+	border-radius: var(--portal-radius);
+	box-shadow: var(--portal-shadow);
+	display: flex;
+	flex-direction: column;
+	overflow: hidden;
+	border: 1px solid #f1f5f9;
 }
-    .support_module .content:hover::-webkit-scrollbar-thumb{
-        background:rgba(0,0,0,0.1);
-    }
-    .support_module .bubble{
-        max-width: 400px;
-        padding: 10px;
-        border-radius: 5px;
-        position: relative;
-        color: #000;
-        word-wrap:break-word;
-        word-break:normal;
-    }
-    .support_module .item_left .bubble{
-        margin-left: 15px;
-        background-color: #fff;
-    }
-    .support_module .item_left .bubble:before{
-        content: "";
-        position: absolute;
-        width: 0;
-        height: 0;
-        border-left: 10px solid transparent;
-        border-top: 10px solid transparent;
-        border-right: 10px solid #fff;
-        border-bottom: 10px solid transparent;
-        left: -20px;
-    }
-    .support_module .item_right .bubble{
-        margin-right: 15px;
-        background-color: #9eea6a;
-    }
-    .support_module .item_right .bubble:before{
-        content: "";
-        position: absolute;
-        width: 0;
-        height: 0;
-        border-left: 10px solid #9eea6a;
-        border-top: 10px solid transparent;
-        border-right: 10px solid transparent;
-        border-bottom: 10px solid transparent;
-				left: none;
 
-				        right: -20px;
-    }
-    .support_module .item{
-        margin-top: 15px;
-        display: flex;
-        width: 100%;
-    }
-    .support_module .item.item_right{
-        justify-content: flex-end;
-				min-width: 400px;
-    }
-    .support_module .item.item-center{
-        justify-content: center;
-    }
-    .support_module .item.item-center span{
-        font-size: 12px;
-        padding: 2px 4px;
-        color: #fff;
-        background-color: #dadada;
-        border-radius: 3px;
-        -moz-user-select:none; /*火狐*/
-        -webkit-user-select:none; /*webkit浏览器*/
-        -ms-user-select:none; /*IE10*/
-        -khtml-user-select:none; /*早期浏览器*/
-        user-select:none;
-    }
+.wide-notice {
+	width: 100% !important;
+	height: 120px !important;
+	flex-direction: row !important;
+}
 
-    .support_module .avatar img{
-        width: 42px;
-        height: 42px;
-        border-radius: 50%;
-    }
-    .support_module .input-area{
-        border-top:0.5px solid #e0e0e0;
-        height: 16%;
+.wide-notice .notice_header {
+	width: 150px;
+	border-bottom: none;
+	border-right: 1px solid #f1f5f9;
+}
 
-        background-color: #fff;
-    }
-    .support_module textarea{
-        flex: 1;
-        padding: 10px;
-        font-size: 14px;
-        border: none;
-        overflow-y: auto;
-        overflow-x: hidden;
-        outline:none;
-        resize:none;
-				width: 102%;
-				height: 100%;
-				min-height: 100%;
-				max-height: 100%;
-    }
-    .support_module .button-area{
-      display: flex;
-    height: 50px;
-    line-height: 50px;
-    padding: 5px;
-    justify-content: flex-end;
-    text-align: right;
-    width: 100%;
-		background: #fff;
-    }
-    .support_module .button-area button{
-        width: 80px;
-				height: 40px;
-				line-height: 40px;
-        border: none;
-        outline: none;
-        border-radius: 4px;
-        float: right;
-        cursor: pointer;
-				background: #9eea6a;
-				color: #333;
-				margin: 0px;
-    }
+.wide-notice .notice_body {
+	display: flex;
+	align-items: center;
+}
 
-    /* 设置滚动条的样式 */
-    ::-webkit-scrollbar {
-        width:10px;
-    }
-    /* 滚动槽 */
-    ::-webkit-scrollbar-track {
-        -webkit-box-shadow:inset006pxrgba(0,0,0,0.3);
-        border-radius:8px;
-    }
-    /* 滚动条滑块 */
-    ::-webkit-scrollbar-thumb {
-        border-radius:10px;
-        background:rgba(0,0,0,0);
-        -webkit-box-shadow:inset006pxrgba(0,0,0,0.5);
-    }
+.notice_header {
+	padding: 15px 20px;
+	border-bottom: 1px solid #f1f5f9;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	background: #f8fafc;
+}
 
-	.sw_no {
-	    margin: 0.5rem;
-	    display: flex;
-	    flex-wrap: wrap;
+.notice_title_text {
+	font-weight: 700;
+	color: var(--portal-text-main);
+	font-size: 16px;
+}
+
+.notice_more {
+	font-size: 13px;
+	color: var(--portal-text-muted);
+}
+
+.notice_body {
+	flex: 1;
+	padding: 15px;
+	overflow: hidden;
+}
+
+.unified-content-card {
+	border-radius: var(--portal-radius) !important;
+	border: 1px solid #f1f5f9 !important;
+	box-shadow: var(--portal-shadow) !important;
+}
+
+.content-column {
+	padding: 10px 0;
+}
+
+.section-header.mini {
+	margin-bottom: 20px;
+	padding-bottom: 10px;
+}
+
+.section-header.mini .title {
+	font-size: 18px;
+}
+
+.section-header.mini .title::after {
+	bottom: -12px;
+	width: 40px;
+}
+
+/* Ensure lists don't wrap too much in 1/2 column */
+.list_article_box >>> .diy_card {
+	margin: 0.5rem 0 !important;
+	width: 100% !important;
+}
+
+.list_commodity_information_box >>> .diy_card {
+	margin: 0.5rem 0 !important;
+	width: 100% !important;
+}
+
+@media (max-width: 996px) {
+	.premium-hero {
+		flex-direction: column;
 	}
-
-	.swiper_img {
-	    width: 60%;
-	    height: 18.75rem;
+	.premium-swiper, .premium-notice-card {
+		width: 100%;
+		height: auto;
+		min-height: 250px;
 	}
-
-	.card_notice {
-	    width: 40%;
-	    padding-left: 1rem;
-	    height: 18.75rem;
-	}
-
-	.notice_title {
-	    font-size: 1.2rem;
-	    font-weight: bold;
-	    color: white;
-	    display: flex;
-		justify-content: space-between;
-	    align-items: center;
-	    background-color: #d2d2d2;
-	    height: 3rem;
-	    padding: 0 1.5rem;
-	}
-
-	.article_recommend > * {
-	    display: flex;
-	    justify-content: center;
-	    align-items: center;
-	}
-
-	.article_recommend {
-	    display: block;
-	    margin: 1.5rem 0.5rem;
-	    height: 4rem;
-	    border-radius: 0.5rem;
-	    background-color: var(--color_primary);
-	}
-
-	.diy_recommend > * {
-	    display: flex;
-	    justify-content: center;
-	    align-items: center;
-	}
-
-	.diy_recommend {
-	    display: block;
-	    margin: 1.5rem 0.5rem;
-	    height: 4rem;
-	    border-radius: 0.5rem;
-	    background-color: var(--color_primary);
-	}
-
-	.title {
-	    float: left;
-	    height: 4rem;
-	    color: white;
-	    font-size: 1.5rem;
-	    font-weight: bold;
-	    padding-left: 1rem;
-	}
-
-	.more_box {
-	    float: right;
-	    height: 4rem;
-	    color: white;
-	    font-size: 1.5rem;
-	    padding-right: 1rem;
-	}
-
-	.more_box .more {
-	    border-radius: 0.5rem;
-	    border: 0.125rem solid white;
-	    display: flex;
-	    align-items: center;
-	    font-size: 1.2rem;
-	    justify-content: center;
-	    height: 3rem;
-	    width: 3.5rem;
-	}
-
-	.switch {
-	    float: right;
-	    height: 4rem;
-	    color: white;
-	    padding-right: 1rem;
-	}
-
-	.switch_box {
-	    border-radius: 0.5rem;
-	    border: 0.125rem solid white;
-	    display: flex;
-	    align-items: center;
-	    font-size: 1.2rem;
-	    justify-content: center;
-	    height: 3rem;
-	    width: 6rem;
-	}
-
-	.switch_box:hover, .more:hover {
-	    background-color: white;
-	    color: var(--color_primary) !important;
-	}
-
-	.overflow-auto .table th,
-	.overflow-auto .table td {
-	    text-align: center;
-	    text-wrap: normal;
-	    font-size: 1rem;
-	    vertical-align: middle;
-	}
-
-	/*.page_index {*/
-	/*    margin: 0 auto;*/
-	/*    max-width: 1200px;*/
-	/*}*/
-
-	/*.main {*/
-	/*    display: flex;*/
-	/*    justify-content: space-between;*/
-	/*}*/
-
-	/*.content {*/
-	/*    flex: 0 0 79%;*/
-	/*}*/
-
-	/*.aside {*/
-	/*    flex: 0 0 20%;*/
-	/*}*/
-
-	.card_ad {
-	    margin-top: 1rem;
-	}
-
-	@media (max-width: 996px) {
-	    .swiper_img {
-	        width: 100%;
-	        height: 12.5rem;
-	    }
-
-	    .card_notice {
-	        width: 100%;
-	        padding-left: 0;
-	    }
-	}
+}
 </style>
